8b1fc057f25ac8b4ba8adce5a2c484fa12d66987
Nicolas Bernin <nicolas.bernin@reccat.io>
2025-04-20 10:03:39 +0200

    add collision out of map

--- global.gd
+++ global.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var player_current_attack = false
+
+var current_scene = "world"
+var transition_scene = false
+
+var player_exit_cliffside_position_x = 0
+var player_exit_cliffside_position_y = 0
+var player_start_position_x = 0
+var player_start_position_y = 0
